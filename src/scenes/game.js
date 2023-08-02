9b2318728fec6cbca8bd5d8e35d90305dac4e290
Bob Streeter <streeterb@bell.net>
2023-08-02 09:11:40 -0400

    update

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -623,42 +623,53 @@ socket.on('updateName', (data, playernum) => {
     let yellowBlueMarbles = (obj, playersmarble) => {
       //console.log('yellowBlue Marble FUNCTION: ', obj.frame.name, playersmarble.frame.name)
 
-      const bhfWithBlue = this.physics.world.overlap([r1, r2, r3, r4, r5], RightHome)
-      const bhfWithYellow = this.physics.world.overlap([l1, l2, l3, l4, l5], RightHome)
-      const bhfWithRed = this.physics.world.overlap([b1, b2, b3, b4, b5], RightHome)
-      const bhfWithGreen = this.physics.world.overlap([t1, t2, t3, t4, t5], RightHome)
-      const yhfWithBlue = this.physics.world.overlap([r1, r2, r3, r4, r5], LeftHome)
-      const yhfWithYellow = this.physics.world.overlap([l1, l2, l3, l4, l5], LeftHome)
-      const yhfWithRed = this.physics.world.overlap([b1, b2, b3, b4, b5], LeftHome)
-      const yhfWithGreen = this.physics.world.overlap([t1, t2, t3, t4, t5], LeftHome)
+      const checkOverlap = (marbles, home) => {
+        return this.physics.world.overlap(marbles, home);
+      };
+      const blueMarbles = [r1, r2, r3, r4, r5];
+      const yellowMarbles = [l1, l2, l3, l4, l5];
+      const redMarbles = [b1, b2, b3, b4, b5];
+      const greenMarbles = [t1, t2, t3, t4, t5];
 
-      if (obj.frame.name === playersmarble.frame.name) {
-         objdragStart(obj)
-            } else if (obj.frame.name === 'b' && playersmarble.frame.name === 'y' && yhfWithYellow) {
+      const bhfWithBlue = checkOverlap(blueMarbles, RightHome);
+      const bhfWithYellow = checkOverlap(yellowMarbles, RightHome);
+      const bhfWithRed = checkOverlap(redMarbles, RightHome);
+      const bhfWithGreen = checkOverlap(greenMarbles, RightHome);
+      const yhfWithBlue = checkOverlap(blueMarbles, LeftHome);
+      const yhfWithYellow = checkOverlap(yellowMarbles, LeftHome);
+      const yhfWithRed = checkOverlap(redMarbles, LeftHome);
+      const yhfWithGreen = checkOverlap(greenMarbles, LeftHome);
+
+      const currentMarble = playersmarble.frame.name;
+      const objName = obj.frame.name;
+
+      if (objName === currentMarble) {
+         objdragStart(obj, this.textMarker)
+            } else if (objName === 'b' && currentMarble === 'y' && yhfWithYellow) {
                 modal.setMessage('Yellow Home entrance is occupied with a Yellow Marble already.\n');
                 modal.show();
-                objdragStart(obj)
-            } else if (obj.frame.name === 'y' && playersmarble.frame.name === 'b' && bhfWithBlue) {
+                objdragStart(obj, this.textMarker)
+            } else if (objName === 'y' && currentMarble === 'b' && bhfWithBlue) {
                     modal.setMessage('Blue Home entrance is occupied with a Blue Marble already.\n');
                     modal.show();
-                    objdragStart(obj)
-            } else if (obj.frame.name === 'b' && playersmarble.frame.name === 'y' && bhfWithYellow && yhfWithBlue) {
+                    objdragStart(obj, this.textMarker)
+            } else if (objName === 'b' && currentMarble === 'y' && bhfWithYellow && yhfWithBlue) {
                         modal.setMessage('Blue Home entrance is blocked as Blue marble at Yellow Home entrance is also block by Yellow marble at the Blue Home entrance.\n');
                         modal.show();
-                        objdragStart(obj)
-            } else if (obj.frame.name === 'y' && playersmarble.frame.name === 'b' && bhfWithYellow && yhfWithBlue) {
+                        objdragStart(obj, this.textMarker)
+            } else if (objName === 'y' && currentMarble === 'b' && bhfWithYellow && yhfWithBlue) {
                             modal.setMessage('Yellow Home entrance is blocked as Yellow marble at Blue Home entrance is also block by Blue marble at the Yellow Home entrance.\n');
                             modal.show();
-                            objdragStart(obj)
-            } else if (obj.frame.name === 'b' && playersmarble.frame.name === 'y' && bhfWithBlue && yhfWithBlue) {
+                            objdragStart(obj, this.textMarker)
+            } else if (objName === 'b' && currentMarble === 'y' && bhfWithBlue && yhfWithBlue) {
                                 modal.setMessage('Blue Home entrance and Yellow Home entrance are both occupied with BLUE Marbles.\n');
                                 modal.show();
-                                objdragStart(obj)
-            } else if (obj.frame.name === 'y' && playersmarble.frame.name === 'b' && bhfWithYellow && yhfWithYellow) {
+                                objdragStart(obj, this.textMarker)
+            } else if (objName === 'y' && currentMarble === 'b' && bhfWithYellow && yhfWithYellow) {
                                     modal.setMessage('Yellow Home entrance and Blue Home entrance are both occupied with YELLOW Marbles.\n');
                                     modal.show();
-                                    objdragStart(obj)
-            } else if (obj.frame.name === 'y' && playersmarble.frame.name === 'b') {
+                                    objdragStart(obj, this.textMarker)
+            } else if (objName === 'y' && currentMarble === 'b') {
                                         if (bhfWithYellow) {
                                             this.physics.world.overlap([l1, l2, l3, l4, l5], RightHome, move_partners_Marble)
                                             this.physics.world.overlap([t1, t2, t3, t4, t5], LeftHome, move_opponets_Marble)
@@ -669,7 +680,7 @@ socket.on('updateName', (data, playernum) => {
                                              this.physics.world.overlap([t1, t2, t3, t4, t5], RightHome, move_opponets_Marble)
                                         }
                                         playerHome(playersmarble, obj)
-            } else if (obj.frame.name === 'b' && playersmarble.frame.name === 'y') {
+            } else if (objName === 'b' && currentMarble === 'y') {
                                             if (yhfWithBlue) {
                                                 this.physics.world.overlap([r1, r2, r3, r4, r5], LeftHome, move_partners_Marble)
                                                 this.physics.world.overlap([b1, b2, b3, b4, b5], RightHome, move_opponets_Marble)
@@ -680,7 +691,7 @@ socket.on('updateName', (data, playernum) => {
                                                 this.physics.world.overlap([t1, t2, t3, t4, t5], LeftHome, move_opponets_Marble)
                                             }
                                             playerHome(playersmarble, obj)
-            } else if ((obj.frame.name === 'g' || obj.frame.name === 'r') && (playersmarble.frame.name === 'b' || playersmarble.frame.name === 'y')) {
+            } else if ((objName === 'g' || obj.frame.name === 'r') && (currentMarble === 'b' || currentMarble === 'y')) {
                                                 playerStart(playersmarble, obj)
                                             }
     } // end of yellowBlueMarbles
@@ -698,31 +709,31 @@ socket.on('updateName', (data, playernum) => {
       const ghfWithGreen = this.physics.world.overlap([t1, t2, t3, t4, t5], TopHome)
 
       if (obj.frame.name === playersmarble.frame.name) {
-                objdragStart(obj)
+                objdragStart(obj, this.textMarker)
             } else if (obj.frame.name === 'g' && playersmarble.frame.name === 'r' && rhfWithRed) {
                         modal.setMessage('Red Home entrance is occupied with a RED Marble already.\n');
                         modal.show();
-                        objdragStart(obj)
+                        objdragStart(obj, this.textMarker)
             } else if (obj.frame.name === 'r' && playersmarble.frame.name === 'g' && ghfWithGreen) {
                         modal.setMessage('Green Home entrance is occupied with a GREEN Marble already.\n');
                         modal.show();
-                        objdragStart(obj)
+                        objdragStart(obj, this.textMarker)
             } else if (obj.frame.name === 'g' && playersmarble.frame.name === 'r' && ghfWithRed && rhfWithGreen) {
                         modal.setMessage('Green Home entrance is blocked as Green marble at Red Home entrance is also block by Red marble at the Green Home entrance.\n');
                         modal.show();
-                        objdragStart(obj)
+                        objdragStart(obj, this.textMarker)
             } else if (obj.frame.name === 'r' && playersmarble.frame.name === 'g' && ghfWithRed && rhfWithGreen) {
                         modal.setMessage('Red Home entrance is blocked as Red marble at Green Home entrance is also block by Green marble at the Red Home entrance.\n');
                         modal.show();
-                        objdragStart(obj)
+                        objdragStart(obj, this.textMarker)
             } else if (obj.frame.name === 'g' && playersmarble.frame.name === 'r' && ghfWithGreen && rhfWithGreen) {
                         modal.setMessage('Green Home entrance and Red Home entrance are both occupied with GREEN Marbles.\n');
                         modal.show();
-                        objdragStart(obj)
+                        objdragStart(obj, this.textMarker)
             } else if (obj.frame.name === 'r' && playersmarble.frame.name === 'g' && ghfWithRed && rhfWithRed) {
                         modal.setMessage('Red Home entrance and Green Home entrance are both occupied with RED Marbles.\n');
                         modal.show();
-                        objdragStart(obj)
+                        objdragStart(obj, this.textMarker)
             } else if (obj.frame.name === 'r' && playersmarble.frame.name === 'g') {
                         // Check to see if red home is occupied, if so move marble
                         if (ghfWithRed) {
@@ -754,9 +765,10 @@ socket.on('updateName', (data, playernum) => {
     } // end of greenRedMarbles
 
 
-    function objdragStart(obj) {
+    function objdragStart(obj, text) {
         obj.x = obj.input.dragStartX
         obj.y = obj.input.dragStartY
+        text.setVisible(false);
       }
 
     function move_partners_Marble(item) {
